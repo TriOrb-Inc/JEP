4582056b8f37a8532b7d07d2e7a9eb71c4996c80
TriOrb <109260078+TriOrb-Inc@users.noreply.github.com>
2024-06-11 15:26:37 +0900

    Update install_opencv4.8.0_Jetson_cuda.sh

--- script/install_opencv4.8.0_Jetson_cuda.sh
+++ script/install_opencv4.8.0_Jetson_cuda.sh
@@ -42,10 +42,10 @@ sudo apt-get install -y curl unzip gcc-10 g++-10
 export CC=/usr/bin/gcc-10
 export CXX=/usr/bin/g++-10
 export CUDA_ROOT=/usr/local/cuda
-unlink $CUDA_ROOT/bin/gcc
-unlink $CUDA_ROOT/bin/g++
-ln -s /usr/bin/gcc-10 $CUDA_ROOT/bin/gcc
-ln -s /usr/bin/g++-10 $CUDA_ROOT/bin/g++
+sudo unlink $CUDA_ROOT/bin/gcc
+sudo unlink $CUDA_ROOT/bin/g++
+sudo ln -s /usr/bin/gcc-10 $CUDA_ROOT/bin/gcc
+suo ln -s /usr/bin/g++-10 $CUDA_ROOT/bin/g++
 
 
 echo "------------------------------------"
